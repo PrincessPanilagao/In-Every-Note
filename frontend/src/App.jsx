84654b99568beded4530f782c9e3ca9e763eb1a5
psjasmin <155319404+PrincessPanilagao@users.noreply.github.com>
2025-05-27 16:27:21 +0400

    Configure template paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>Subscribe to me ayo</div>
+    <div className="text-red-500 text-xl font-semibold">Subscribe to me ayo</div>
   )
 }
 
