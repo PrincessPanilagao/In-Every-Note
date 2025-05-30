e56dfcaa10f69995db4e10b464cb31010bea71cd
psjasmin <155319404+PrincessPanilagao@users.noreply.github.com>
2025-05-30 08:47:47 +0400

    Update Redux authentication
    User cannot log/sign back in after login is successful

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,11 +2,15 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import axios from "axios";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { authActions } from "../store/auth";
+import ErrorPage from "./ErrorPage";
 
 // ---LOGIN PAGE---
 const Login = () => {
+  // check state
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
   // Connecting sign-in page to backend - storing inputted values
@@ -38,81 +42,89 @@ const Login = () => {
       toast.error(error.response.data.message);
     }
   };
+
+  // note: user cannot log back in after logging in
   return (
-    <div className="bg-[url('/src/assets/login-bg.png')] bg-no-repeat bg-center bg-cover min-h-screen w-full h-screen flex items-center justify-center">
-      {/* Toast alert container */}
-      <ToastContainer
-        position="top-right"
-        draggable
-        theme="colored"
-        toastClassName="custom-toast"
-      />
-      <div className="w-4/6 md:w-4/6 lg:w-[27%] flex flex-col items-center justify-center">
-        <Link
-          to="/"
-          className="text-5xl sm:text-6xl md:text-8xl lg:text-7xl text-[#9A2B2E] font-taprom"
-        >
-          in every note
-        </Link>
-        {/* HTML form for login page */}
-        <div className="mt-6 w-full font-worksans items-center justify-center">
-          {/* EMAIL */}
-          <div className="w-full flex flex-col mt-6">
-            <label htmlFor="" className="font-medium">
-              Email
-            </label>
-            <input
-              type="email"
-              className="mt-2 px-2 py-2 rounded outline-none border-2 border-[#262424] focus:bg-[#FFF2E9] bg-[#F8F2ED]"
-              required
-              placeholder="Email"
-              name="email"
-              value={Values.email}
-              onChange={change}
-            />
-          </div>
-
-          {/* PASSWORD */}
-          <div className="w-full flex flex-col mt-6">
-            <label htmlFor="" className="font-medium">
-              Password
-            </label>
-            <input
-              type="password"
-              className="mt-2 px-2 py-2 rounded outline-none border-2 border-[#262424] focus:bg-[#FFF2E9] bg-[#F8F2ED]"
-              required
-              placeholder="Password"
-              name="password"
-              value={Values.password}
-              onChange={change}
-            />
-          </div>
-
-          {/* LOGIN BUTTON */}
-          <div className="w-36 mx-auto flex flex-col mt-8">
-            <button
-              className="font-worksans font-medium px-1 py-2 bg-[#9A2B2E] text-[#FCFAF9] border-2 border-[#262424] rounded-full duration-300 hover:bg-[#F8F2ED] hover:text-[#262424]"
-              onClick={handleSubmit}
+    <>
+      {isLoggedIn ? (
+        <ErrorPage />
+      ) : (
+        <div className="bg-[url('/src/assets/login-bg.png')] bg-no-repeat bg-center bg-cover min-h-screen w-full h-screen flex items-center justify-center">
+          {/* Toast alert container */}
+          <ToastContainer
+            position="top-right"
+            draggable
+            theme="colored"
+            toastClassName="custom-toast"
+          />
+          <div className="w-4/6 md:w-4/6 lg:w-[27%] flex flex-col items-center justify-center">
+            <Link
+              to="/"
+              className="text-5xl sm:text-6xl md:text-8xl lg:text-7xl text-[#9A2B2E] font-taprom"
             >
-              LOG IN
-            </button>
-          </div>
+              in every note
+            </Link>
+            {/* HTML form for login page */}
+            <div className="mt-6 w-full font-worksans items-center justify-center">
+              {/* EMAIL */}
+              <div className="w-full flex flex-col mt-6">
+                <label htmlFor="" className="font-medium">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="mt-2 px-2 py-2 rounded outline-none border-2 border-[#262424] focus:bg-[#FFF2E9] bg-[#F8F2ED]"
+                  required
+                  placeholder="Email"
+                  name="email"
+                  value={Values.email}
+                  onChange={change}
+                />
+              </div>
 
-          {/* SIGNUP CTA */}
-          <div className="w-full flex flex-col mt-8">
-            <p className="text-center font-medium">
-              Don't have an account?{" "}
-              <Link
-                to="/signup"
-                className="font-semibold text-[#9A2B2E] hover:text-[#262424]"
-              >
-                Sign up
-              </Link>
-            </p>
+              {/* PASSWORD */}
+              <div className="w-full flex flex-col mt-6">
+                <label htmlFor="" className="font-medium">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="mt-2 px-2 py-2 rounded outline-none border-2 border-[#262424] focus:bg-[#FFF2E9] bg-[#F8F2ED]"
+                  required
+                  placeholder="Password"
+                  name="password"
+                  value={Values.password}
+                  onChange={change}
+                />
+              </div>
+
+              {/* LOGIN BUTTON */}
+              <div className="w-36 mx-auto flex flex-col mt-8">
+                <button
+                  className="font-worksans font-medium px-1 py-2 bg-[#9A2B2E] text-[#FCFAF9] border-2 border-[#262424] rounded-full duration-300 hover:bg-[#F8F2ED] hover:text-[#262424]"
+                  onClick={handleSubmit}
+                >
+                  LOG IN
+                </button>
+              </div>
+
+              {/* SIGNUP CTA */}
+              <div className="w-full flex flex-col mt-8">
+                <p className="text-center font-medium">
+                  Don't have an account?{" "}
+                  <Link
+                    to="/signup"
+                    className="font-semibold text-[#9A2B2E] hover:text-[#262424]"
+                  >
+                    Sign up
+                  </Link>
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -3,9 +3,14 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import axios from "axios";
+import { useSelector } from "react-redux";
+import ErrorPage from "./ErrorPage";
 
 // ---SIGNUP PAGE---
 const Signup = () => {
+  // check state
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
   const navigate = useNavigate();
   // Connecting sign-up page to backend - storing inputted values
   const [Values, setValues] = useState({
@@ -30,103 +35,111 @@ const Signup = () => {
       // Showing data message success alert
       console.log(res.data.message);
       // direct to login when successful
-      navigate("/login"); 
+      navigate("/login");
     } catch (error) {
       // Showing data message rror/warning alert - using toast
       toast.error(error.response.data.message);
     }
   };
+
+  // note: user cannot sign back in after logging in
   return (
-    <div className="bg-[url('/src/assets/signup-bg.png')] bg-no-repeat bg-center bg-cover min-h-screen w-full h-screen flex items-center justify-center">
-      {/* Toast alert container */}
-      <ToastContainer
-        position="top-right"
-        draggable
-        theme="colored"
-        toastClassName="custom-toast"
-      />
-      <div className="w-4/6 md:w-3/6 lg:w-[27%] flex flex-col items-center justify-center">
-        <Link
-          to="/"
-          className="text-5xl sm:text-6xl md:text-8xl lg:text-7xl text-[#9A2B2E] font-taprom"
-        >
-          in every note
-        </Link>
-        {/* HTML form for signup page */}
-        <div className="mt-6 w-full font-worksans items-center justify-center">
-          {/* USERNAME */}
-          <div className="w-full flex flex-col lg:mt-7">
-            <label htmlFor="" className="font-medium">
-              Username
-            </label>
-            <input
-              type="text"
-              className="mt-2 px-2 py-2 rounded outline-none border-2 border-[#262424] focus:bg-[#FFF2E9] bg-[#F8F2ED]"
-              required
-              placeholder="Username"
-              name="username"
-              value={Values.username}
-              onChange={change}
-            />
-          </div>
-
-          {/* EMAIL */}
-          <div className="w-full flex flex-col mt-6">
-            <label htmlFor="" className="font-medium">
-              Email
-            </label>
-            <input
-              type="email"
-              className="mt-2 px-2 py-2 rounded outline-none border-2 border-[#262424] focus:bg-[#FFF2E9] bg-[#F8F2ED]"
-              required
-              placeholder="Email"
-              name="email"
-              value={Values.email}
-              onChange={change}
-            />
-          </div>
-
-          {/* PASSWORD */}
-          <div className="w-full flex flex-col mt-6">
-            <label htmlFor="" className="font-medium">
-              Password
-            </label>
-            <input
-              type="password"
-              className="mt-2 px-2 py-2 rounded outline-none border-2 border-[#262424] focus:bg-[#FFF2E9] bg-[#F8F2ED]"
-              required
-              placeholder="Password"
-              name="password"
-              value={Values.password}
-              onChange={change}
-            />
-          </div>
-
-          {/* SIGNUP BUTTON */}
-          <div className="w-36 mx-auto flex flex-col mt-8">
-            <button
-              className="font-worksans font-medium px-1 py-2 bg-[#9A2B2E] text-[#FCFAF9] border-2 border-[#262424] rounded-full duration-300 hover:bg-[#F8F2ED] hover:text-[#262424]"
-              onClick={handleSubmit}
+    <>
+      {isLoggedIn ? (
+        <ErrorPage />
+      ) : (
+        <div className="bg-[url('/src/assets/signup-bg.png')] bg-no-repeat bg-center bg-cover min-h-screen w-full h-screen flex items-center justify-center">
+          {/* Toast alert container */}
+          <ToastContainer
+            position="top-right"
+            draggable
+            theme="colored"
+            toastClassName="custom-toast"
+          />
+          <div className="w-4/6 md:w-3/6 lg:w-[27%] flex flex-col items-center justify-center">
+            <Link
+              to="/"
+              className="text-5xl sm:text-6xl md:text-8xl lg:text-7xl text-[#9A2B2E] font-taprom"
             >
-              SIGN UP
-            </button>
-          </div>
+              in every note
+            </Link>
+            {/* HTML form for signup page */}
+            <div className="mt-6 w-full font-worksans items-center justify-center">
+              {/* USERNAME */}
+              <div className="w-full flex flex-col lg:mt-7">
+                <label htmlFor="" className="font-medium">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  className="mt-2 px-2 py-2 rounded outline-none border-2 border-[#262424] focus:bg-[#FFF2E9] bg-[#F8F2ED]"
+                  required
+                  placeholder="Username"
+                  name="username"
+                  value={Values.username}
+                  onChange={change}
+                />
+              </div>
 
-          {/* SIGNUP BUTTON */}
-          <div className="w-full flex flex-col mt-8">
-            <p className="text-center font-medium">
-              Already have an account?{" "}
-              <Link
-                to="/login"
-                className="font-semibold text-[#9A2B2E] hover:text-[#262424]"
-              >
-                Log in
-              </Link>
-            </p>
+              {/* EMAIL */}
+              <div className="w-full flex flex-col mt-6">
+                <label htmlFor="" className="font-medium">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="mt-2 px-2 py-2 rounded outline-none border-2 border-[#262424] focus:bg-[#FFF2E9] bg-[#F8F2ED]"
+                  required
+                  placeholder="Email"
+                  name="email"
+                  value={Values.email}
+                  onChange={change}
+                />
+              </div>
+
+              {/* PASSWORD */}
+              <div className="w-full flex flex-col mt-6">
+                <label htmlFor="" className="font-medium">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="mt-2 px-2 py-2 rounded outline-none border-2 border-[#262424] focus:bg-[#FFF2E9] bg-[#F8F2ED]"
+                  required
+                  placeholder="Password"
+                  name="password"
+                  value={Values.password}
+                  onChange={change}
+                />
+              </div>
+
+              {/* SIGNUP BUTTON */}
+              <div className="w-36 mx-auto flex flex-col mt-8">
+                <button
+                  className="font-worksans font-medium px-1 py-2 bg-[#9A2B2E] text-[#FCFAF9] border-2 border-[#262424] rounded-full duration-300 hover:bg-[#F8F2ED] hover:text-[#262424]"
+                  onClick={handleSubmit}
+                >
+                  SIGN UP
+                </button>
+              </div>
+
+              {/* SIGNUP BUTTON */}
+              <div className="w-full flex flex-col mt-8">
+                <p className="text-center font-medium">
+                  Already have an account?{" "}
+                  <Link
+                    to="/login"
+                    className="font-semibold text-[#9A2B2E] hover:text-[#262424]"
+                  >
+                    Log in
+                  </Link>
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
