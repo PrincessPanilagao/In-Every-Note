271eaf5d6003b10c271f8855944e5131ea952b0b
psjasmin <155319404+PrincessPanilagao@users.noreply.github.com>
2025-05-30 16:03:38 +0400

    Changed IEN logo

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -66,9 +66,9 @@ const Navbar = () => {
         <div className="flex justify-center lg:flex-1">
           <Link to="/">
             <img
-              src="src/assets/logo.png"
+              src="src/assets/ien-logo.png"
               alt="In Every Note Logo"
-              className="w-10 sm:w-12 md:w-14 h-auto"
+              className="w-11 sm:w-12 md:w-14 h-auto"
             />
           </Link>
         </div>
