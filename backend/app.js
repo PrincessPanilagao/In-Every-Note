0a631fc6740a8d546abf63f2c9aa2dae49b0d144
Princess Jasmin Panilagao <155319404+PrincessPanilagao@users.noreply.github.com>
2025-06-01 21:48:56 +0400

    Update app.js origin

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ require("./conn/conn");
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"], // backend giving access to frontend origin 
+    origin: ["https://in-every-note-frontend.onrender.com"], // backend giving access to frontend origin - http://localhost:5173
     credentials: true, // storing credentials from frontend to backend
   })
 );
